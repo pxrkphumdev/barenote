df33a907d695c442d438720984a73aca575609f3
Parkphum Poosuthasee <89057663+parkphumdev@users.noreply.github.com>
2025-01-02 22:28:36 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -131,3 +131,17 @@ document.querySelector('#note-list').addEventListener('click', (e) => {
   Store.removeNote(e.target.parentElement.parentElement.firstElementChild.textContent)
 })
 
+
+function updateDateTime() {
+  // create a new `Date` object
+  const now = new Date();
+
+  // get the current date and time as a string
+  const currentDateTime = now.toLocaleString();
+
+  // update the `textContent` property of the `span` element with the `id` of `datetime`
+  document.querySelector('#datetime').textContent = currentDateTime;
+}
+
+// call the `updateDateTime` function every second
+setInterval(updateDateTime, 1000);
