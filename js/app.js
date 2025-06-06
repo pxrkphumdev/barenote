f8af640ed342c8fcbbcab93eae83feb9807d2f24
pxrkphumdev <parkphum.dev@gmail.com>
2025-06-06 12:27:20 +0700

    change time and table-css

--- js/app.js
+++ js/app.js
@@ -2,8 +2,7 @@
 class Note {
   constructor(id, text, cost) {
     this.id = id
-    this.text = note,
-      this.cost = cost
+    ;(this.text = note), (this.cost = cost)
   }
 }
 
@@ -11,7 +10,9 @@ class Note {
 class Datetime {
   static getFullTime(date) {
     const d = new Date(date)
-    return `${d.getHours()}:${(d.getMinutes() < 10 ? '0' : '') + d.getMinutes()}`
+    return `${d.getHours()}:${
+      (d.getMinutes() < 10 ? "0" : "") + d.getMinutes()
+    }`
   }
 }
 
@@ -21,7 +22,7 @@ class UI {
   static displayName() {
     const name = Store.getName()
 
-    const el = document.querySelector('#name')
+    const el = document.querySelector("#name")
     el.textContent = name
   }
 
@@ -30,13 +31,16 @@ class UI {
     const notes = Store.getNotes()
     let total = 0
 
-    total = notes.reduce((sum, note) => sum + parseInt(note.cost.toString().replace(/\D/g, '')), 0)
-    document.querySelector('#total').textContent = '฿ ' + total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    total = notes.reduce(
+      (sum, note) => sum + parseInt(note.cost.toString().replace(/\D/g, "")),
+      0
+    )
+    document.querySelector("#total").textContent =
+      "฿ " + total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
   }
 
   // Display note list
   static displayNotes() {
-
     // From localStorage
     const notes = Store.getNotes()
 
@@ -45,13 +49,20 @@ class UI {
 
   // Add note UI
   static addNoteToList(note) {
-    const list = document.querySelector('#note-list')
-    const row = document.createElement('tr')
+    const list = document.querySelector("#note-list")
+    const row = document.createElement("tr")
+    row.classList.add("odd:bg-white")
+    row.classList.add("even:bg-gray-50")
+    row.classList.add("border-b")
+    row.classList.add("border-gray-200")
+
     row.innerHTML = `
       <th scope="row">${note.id}</th>
       <td class="cost" class="d-inline-flex text-nowrap">${note.cost}</td>
-      <td class="note">
-       <p class="font-light text-xs italic text-gray-500">[${Datetime.getFullTime(note.time.start)} , ${Datetime.getFullTime(note.time.stop)}]</p>
+      <td class="note text-left">
+       <p class="font-light text-xs italic text-gray-500">[${Datetime.getFullTime(
+         note.time.start
+       )} , ${Datetime.getFullTime(note.time.stop)}]</p>
        <p class="text">${note.text}</p>
       </td>
       <td><button type="button" class="bg-red-500 text-white px-2 rounded-full delete" type="button">X</button></td>
@@ -61,42 +72,47 @@ class UI {
 
   // Update a note
   static updateNote(el) {
-
     let id = el.parentElement.firstElementChild.textContent
-    if (el.classList.contains('text')) {
+    if (el.classList.contains("text")) {
       id = el.parentElement.parentElement.firstElementChild.textContent
     }
 
     // console.log(id)
 
     const notes = Store.getNotes()
-    const note = notes.filter(e => e.id == id)[0]
+    const note = notes.filter((e) => e.id == id)[0]
 
     let newNote = note
 
     // console.log(notes)
     // console.log(newNote)
 
-    if (el.classList.contains('text')) {
+    if (el.classList.contains("text")) {
       // console.log(el.textContent)
-      const text = prompt('How change ?', el.textContent)
-      const finalText = (text == null ? 'Please note something!' : text)
+      const text = prompt("How change ?", el.textContent)
+      const finalText = text == null ? "Please note something!" : text
       el.textContent = finalText
 
       newNote = { ...note, text: finalText }
     }
 
-    if (el.classList.contains('cost')) {
+    if (el.classList.contains("cost")) {
       // console.log(el.textContent)
       // const cost = prompt('How much ?', el.textContent)
-      const cost = '฿ ' + prompt('How much ?', el.textContent).replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-      const newCost = cost.toString().replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      const cost =
+        "฿ " +
+        prompt("How much ?", el.textContent)
+          .replace(/\D/g, "")
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      const newCost = cost
+        .toString()
+        .replace(/\D/g, "")
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
       el.textContent = cost
       console.log(cost)
       console.log(newCost)
 
       console.log(newNote)
-
 
       newNote = { ...note, cost: newCost }
     }
@@ -107,14 +123,14 @@ class UI {
 
   // Delete a note
   static deleteNote(el) {
-    if (el.classList.contains('delete')) {
+    if (el.classList.contains("delete")) {
       el.parentElement.parentElement.remove()
     }
   }
 
   static clearFiled() {
-    document.getElementById('timestart').innerHTML = ''
-    document.getElementById('timestop').innerHTML = ''
+    document.getElementById("timestart").innerHTML = ""
+    document.getElementById("timestop").innerHTML = ""
   }
 }
 
@@ -124,38 +140,45 @@ class Store {
     const notes = Store.getNotes()
     let total = 0
 
-    total = notes.reduce((sum, note) => sum + parseInt(note.cost.toString().replace(/\D/g, '')), 0)
-    document.querySelector('#total').textContent = '฿ ' + total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    total = notes.reduce(
+      (sum, note) => sum + parseInt(note.cost.toString().replace(/\D/g, "")),
+      0
+    )
+    document.querySelector("#total").textContent =
+      "฿ " + total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
-    localStorage.setItem('total', JSON.stringify(total))
+    localStorage.setItem("total", JSON.stringify(total))
   }
 
   // Name store
   static getName() {
     let name
-    if (localStorage.getItem('name') === null || JSON.parse(localStorage.getItem('name')) == "") {
-      name = 'Quickly and easily note with Barenote.'
+    if (
+      localStorage.getItem("name") === null ||
+      JSON.parse(localStorage.getItem("name")) == ""
+    ) {
+      name = "Quickly and easily note with Barenote."
     } else {
-      name = JSON.parse(localStorage.getItem('name'))
+      name = JSON.parse(localStorage.getItem("name"))
     }
     return name
   }
 
   static setName(name) {
-    if (name !== '') {
-      localStorage.setItem('name', JSON.stringify(name))
+    if (name !== "") {
+      localStorage.setItem("name", JSON.stringify(name))
     } else {
-      localStorage.setItem('name', JSON.stringify('Clinic somewhere'))
+      localStorage.setItem("name", JSON.stringify("Clinic somewhere"))
     }
   }
 
   // Note store
   static getNotes() {
     let notes
-    if (localStorage.getItem('notes') === null) {
+    if (localStorage.getItem("notes") === null) {
       notes = []
     } else {
-      notes = JSON.parse(localStorage.getItem('notes'))
+      notes = JSON.parse(localStorage.getItem("notes"))
     }
     return notes
   }
@@ -163,14 +186,14 @@ class Store {
   static addNote(note) {
     const notes = Store.getNotes()
     notes.push(note)
-    localStorage.setItem('notes', JSON.stringify(notes))
+    localStorage.setItem("notes", JSON.stringify(notes))
   }
 
   static updateNote(id, nnote) {
     // console.log(newNote)
     const notes = Store.getNotes()
     let newNotes = []
-    notes.forEach(note => {
+    notes.forEach((note) => {
       if (note.id == id) {
         newNotes.push(nnote)
       } else {
@@ -178,10 +201,9 @@ class Store {
       }
     })
 
-
     // console.log(newNotes)
 
-    localStorage.setItem('notes', JSON.stringify(newNotes))
+    localStorage.setItem("notes", JSON.stringify(newNotes))
     // console.log({...notes, {id, text: newNote.text, cost: newNote.cost}})
 
     // const nn = {...notes, {id:id, text: newNote.text, cost: newNote.cost}}
@@ -197,13 +219,13 @@ class Store {
       }
     })
 
-    localStorage.setItem('notes', JSON.stringify(notes))
+    localStorage.setItem("notes", JSON.stringify(notes))
   }
 }
 
 // Event: Set name
-document.querySelector('#name').addEventListener('click', () => {
-  const name = prompt('What the name you would like to set ?')
+document.querySelector("#name").addEventListener("click", () => {
+  const name = prompt("What the name you would like to set ?")
 
   // Store
   Store.setName(name)
@@ -213,9 +235,9 @@ document.querySelector('#name').addEventListener('click', () => {
 })
 
 // Event: Display Notes
-document.addEventListener('DOMContentLoaded', UI.displayName)
-document.addEventListener('DOMContentLoaded', UI.displayNotes)
-document.addEventListener('DOMContentLoaded', UI.displayTotal)
+document.addEventListener("DOMContentLoaded", UI.displayName)
+document.addEventListener("DOMContentLoaded", UI.displayNotes)
+document.addEventListener("DOMContentLoaded", UI.displayTotal)
 
 // Event: Add a note
 // document.querySelector('#note-add').addEventListener('click', () => {
@@ -228,7 +250,6 @@ document.addEventListener('DOMContentLoaded', UI.displayTotal)
 
 //   const note = { id, text, cost }
 
-
 //   // Add note to Store
 //   Store.addNote(note)
 //   // Add note to UI
@@ -239,48 +260,72 @@ document.addEventListener('DOMContentLoaded', UI.displayTotal)
 // })
 
 // Event: Update a note
-document.querySelector('#note-list').addEventListener('click', (e) => {
+document.querySelector("#note-list").addEventListener("click", (e) => {
   // Update note from UI
   UI.updateNote(e.target)
 
   // Display total
   UI.displayTotal()
-
 })
 
-
 // Event: Remove a note
-document.querySelector('#note-list').addEventListener('click', (e) => {
-  if (!e.target.classList.contains('delete')) {
+document.querySelector("#note-list").addEventListener("click", (e) => {
+  if (!e.target.classList.contains("delete")) {
     return 0
   }
 
-  if (!confirm('Are your sure to delete this record ?')) {
+  if (!confirm("Are your sure to delete this record ?")) {
     return 0
   }
   // Delete note from UI
 
   // Delete note from Store
-  Store.removeNote(e.target.parentElement.parentElement.firstElementChild.textContent)
+  Store.removeNote(
+    e.target.parentElement.parentElement.firstElementChild.textContent
+  )
   UI.deleteNote(e.target)
 
   // Display total
   UI.displayTotal()
 })
 
+function formatDateTime(date) {
+  const year = ("0" + date.getFullYear()).slice(-4)
+  const month = ("0" + (date.getMonth() + 1)).slice(-2)
+  const day = ("0" + date.getDate()).slice(-2)
+  const hour = ("0" + date.getHours()).slice(-2)
+  const minute = ("0" + date.getMinutes()).slice(-2)
+  const second = ("0" + date.getSeconds()).slice(-2)
+
+  return { year, month, day, hour, minute, second }
+}
 
 function updateDateTime() {
   // create a new `Date` object
-  const now = new Date();
+  const now = new Date()
+
+  const dString = formatDateTime(now)
+  // const year = ("0" + now.getFullYear()).slice(-4);
+  // const month = ("0" + (now.getMonth() + 1)).slice(-2);
+  // const day = ("0" + now.getDate()).slice(-2);
+  // const hour = ("0" + now.getHours()).slice(-2);
+  // const minute = ("0" + now.getMinutes()).slice(-2);
+  // const second = ("0" + now.getSeconds()).slice(-2);
+
+  const currentTime = `<div>${dString.year}-${dString.month}-${dString.day}</div><div>${dString.hour}:${dString.minute}:${dString.second}</div>`
 
   // get the current date and time as a string
-  const currentDateTime = now.toLocaleString()
+  // const currentDateTime = now.toLocaleString();
   const currentDateTimeString = now.toISOString().slice(0, 10)
 
   // update the `textContent` property of the `span` element with the `id` of `datetime`
-  document.querySelector('#datetime').textContent = currentDateTime;
-  document.title = 'Barenote_' + currentDateTimeString + '_' + document.querySelector('#name').innerHTML
+  document.querySelector("#datetime").innerHTML = currentTime
+  document.title =
+    "Barenote_" +
+    `${dString.year}-${dString.month}-${dString.day}` +
+    "_" +
+    document.querySelector("#name").innerHTML
 }
 
 // call the `updateDateTime` function every second
-setInterval(updateDateTime, 1000);
+setInterval(updateDateTime, 1000)
--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,7 @@
 class Countdown {
   constructor(el, TIME_LIMIT) {
     this.el = el
-    this.elId = this.el.getAttribute('id')
+    this.elId = this.el.getAttribute("id")
     this.TIME_LIMIT = TIME_LIMIT
     this.timePassed = 0
     this.timeLeft = this.TIME_LIMIT
@@ -13,19 +13,18 @@ class Countdown {
     this.ALERT_THRESHOLD = this.TIME_LIMIT * 0.33
     this.COLOR_CODES = {
       info: {
-        color: "green"
+        color: "green",
       },
       warning: {
         color: "orange",
-        threshold: this.WARNING_THRESHOLD
+        threshold: this.WARNING_THRESHOLD,
       },
       alert: {
         color: "red",
-        threshold: this.ALERT_THRESHOLD
-      }
+        threshold: this.ALERT_THRESHOLD,
+      },
     }
     this.remainingPathColor = this.COLOR_CODES.info.color
-
 
     this.el.innerHTML = `
     <div class="base-timer">
@@ -52,7 +51,7 @@ class Countdown {
 
   // Format timeLeft
   formatTime() {
-    const minutes = Math.floor(this.timeLeft / 60);
+    const minutes = Math.floor(this.timeLeft / 60)
     let seconds = this.timeLeft % 60
 
     if (seconds < 10) {
@@ -68,10 +67,12 @@ class Countdown {
     // console.log(this.TIME_LIMIT)
     this.elId = setInterval(() => {
       this.timePassed = this.timePassed += 1
-      this.timeLeft = this.TIME_LIMIT - this.timePassed;
-      this.el.querySelector(".base-timer").querySelector("#base-timer-label").innerHTML = this.formatTime()
+      this.timeLeft = this.TIME_LIMIT - this.timePassed
+      this.el
+        .querySelector(".base-timer")
+        .querySelector("#base-timer-label").innerHTML = this.formatTime()
       this.setCircleDasharray()
-      this.setRemainingPathColor();
+      this.setRemainingPathColor()
 
       if (this.timeLeft === 0) {
         clearInterval(this.elId)
@@ -99,7 +100,6 @@ class Countdown {
     clearInterval(this.elId)
     this.timePassed = 0
     new Countdown(this.el, this.TIME_LIMIT)
-
   }
 
   // Calculate Fraction
@@ -110,78 +110,77 @@ class Countdown {
 
   // Set remaining path color
   setRemainingPathColor() {
-    let el = this.el.querySelector(".base-timer").querySelector("svg").querySelector("g").querySelector("path")
+    let el = this.el
+      .querySelector(".base-timer")
+      .querySelector("svg")
+      .querySelector("g")
+      .querySelector("path")
     const { alert, warning, info } = this.COLOR_CODES
     if (this.timeLeft <= 0) {
-      el
-        .classList.remove(alert.color)
-      el
-        .classList.add('grey')
+      el.classList.remove(alert.color)
+      el.classList.add("grey")
     } else if (this.timeLeft <= alert.threshold) {
-      el
-        .classList.remove(warning.color)
-      el
-        .classList.add(alert.color);
+      el.classList.remove(warning.color)
+      el.classList.add(alert.color)
     } else if (this.timeLeft <= warning.threshold) {
-      el
-        .classList.remove(info.color)
-      el
-        .classList.add(warning.color)
+      el.classList.remove(info.color)
+      el.classList.add(warning.color)
     }
   }
 
   // Set remaining path circle
   setCircleDasharray() {
-    let el = this.el.querySelector(".base-timer").querySelector("svg").querySelector("g").querySelector("path")
+    let el = this.el
+      .querySelector(".base-timer")
+      .querySelector("svg")
+      .querySelector("g")
+      .querySelector("path")
     const circleDasharray = `${(
       this.calculateTimeFraction() * this.FULL_DASH_ARRAY
-    ).toFixed(0)} 283`;
-    el
-      .setAttribute("stroke-dasharray", circleDasharray);
+    ).toFixed(0)} 283`
+    el.setAttribute("stroke-dasharray", circleDasharray)
   }
-
 }
 
 // Instantiate Countdown
-const cd1 = new Countdown(document.getElementById('cd1'), 5 * 60)
-const cd2 = new Countdown(document.getElementById('cd2'), 10 * 60)
-const cd3 = new Countdown(document.getElementById('cd3'), 20 * 60)
-const cd4 = new Countdown(document.getElementById('cd4'), 30 * 60)
+const cd1 = new Countdown(document.getElementById("cd1"), 5 * 60)
+const cd2 = new Countdown(document.getElementById("cd2"), 10 * 60)
+const cd3 = new Countdown(document.getElementById("cd3"), 20 * 60)
+const cd4 = new Countdown(document.getElementById("cd4"), 30 * 60)
 
 // Element
-const timestart = document.getElementById('timestart')
-const startShow = document.getElementById('startShow')
-const timestop = document.getElementById('timestop')
-const stopShow = document.getElementById('stopShow')
-const startBtn = document.getElementById('start')
-const stopBtn = document.getElementById('stop')
-const pauseBtn = document.getElementById('pause')
-const continueBtn = document.getElementById('continue')
-const clearBtn = document.getElementById('clear')
-const resetBtn = document.getElementById('reset')
-const cd4el = document.getElementById('cd4')
-const cd5el = document.getElementById('cd5')
-
+const timestart = document.getElementById("timestart")
+const startShow = document.getElementById("startShow")
+const timestop = document.getElementById("timestop")
+const stopShow = document.getElementById("stopShow")
+const startBtn = document.getElementById("start")
+const stopBtn = document.getElementById("stop")
+const pauseBtn = document.getElementById("pause")
+const continueBtn = document.getElementById("continue")
+const clearBtn = document.getElementById("clear")
+const resetBtn = document.getElementById("reset")
+const cd4el = document.getElementById("cd4")
+const cd5el = document.getElementById("cd5")
 
 // Event: Handler
-startBtn.addEventListener('click', () => {
+startBtn.addEventListener("click", () => {
   cd1.startTimer()
   cd2.startTimer()
   cd3.startTimer()
   cd4.startTimer()
   cd5.startTimer()
-  cd4el.classList.remove('hidden')
-  cd5el.classList.remove('hidden')
-  startBtn.classList.add('hidden')
-  stopBtn.classList.remove('hidden')
-  pauseBtn.classList.remove('hidden')
+  cd4el.classList.remove("hidden")
+  cd5el.classList.remove("hidden")
+  startBtn.classList.add("hidden")
+  stopBtn.classList.remove("hidden")
+  pauseBtn.classList.remove("hidden")
 
   d = new Date()
   timestart.innerHTML = d
   // startShow.innerHTML = `START: ${Datetime.getFullTime(d)}`
 })
 
-stopBtn.addEventListener('click', () => {
+stopBtn.addEventListener("click", () => {
   // cd1.pauseTimer()
   // cd2.pauseTimer()
   // cd3.pauseTimer()
@@ -191,24 +190,26 @@ stopBtn.addEventListener('click', () => {
   cd3.resetTimer()
   cd4.resetTimer()
   cd5.resetTimer()
-  cd4el.classList.add('hidden')
-  cd5el.classList.add('hidden')
-  stopBtn.classList.add('hidden')
-  startBtn.classList.remove('hidden')
-  pauseBtn.classList.add('hidden')
+  cd4el.classList.add("hidden")
+  cd5el.classList.add("hidden")
+  stopBtn.classList.add("hidden")
+  startBtn.classList.remove("hidden")
+  pauseBtn.classList.add("hidden")
 
   d = new Date()
   timestop.innerHTML = d
   // stopShow.innerHTML = `STOP: ${Datetime.getFullTime(d)}`
 
-
   // Add new note
-  const id = Store.getNotes().length == 0 ? 1 : parseInt(Store.getNotes()[Store.getNotes().length - 1].id) + 1
+  const id =
+    Store.getNotes().length == 0
+      ? 1
+      : parseInt(Store.getNotes()[Store.getNotes().length - 1].id) + 1
   // console.log(id)
   // const text = prompt('What would you like to note ?')
   // const cost = '฿ ' + prompt('How much ?').replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
-  const text = 'Please note something!'
+  const text = "Please note something!"
   const time = { start: timestart.innerHTML, stop: timestop.innerHTML }
   const cost = 0
   // console.log(cost)
@@ -221,28 +222,27 @@ stopBtn.addEventListener('click', () => {
   UI.addNoteToList(note)
 
   UI.displayTotal()
-
 })
-pauseBtn.addEventListener('click', () => {
+pauseBtn.addEventListener("click", () => {
   cd1.pauseTimer()
   cd2.pauseTimer()
   cd3.pauseTimer()
   cd4.pauseTimer()
   cd5.pauseTimer()
-  pauseBtn.classList.add('hidden')
-  continueBtn.classList.remove('hidden')
+  pauseBtn.classList.add("hidden")
+  continueBtn.classList.remove("hidden")
 })
-continueBtn.addEventListener('click', () => {
+continueBtn.addEventListener("click", () => {
   cd1.continueTimer()
   cd2.continueTimer()
   cd3.continueTimer()
   cd4.continueTimer()
   cd5.continueTimer()
-  continueBtn.classList.add('hidden')
-  pauseBtn.classList.remove('hidden')
+  continueBtn.classList.add("hidden")
+  pauseBtn.classList.remove("hidden")
 })
-clearBtn.addEventListener('click', () => {
-  if (confirm('Are you sure ?')) {
+clearBtn.addEventListener("click", () => {
+  if (confirm("Are you sure ?")) {
     cd1.resetTimer()
     cd2.resetTimer()
     cd3.resetTimer()
@@ -250,10 +250,9 @@ clearBtn.addEventListener('click', () => {
     cd5.resetTimer()
 
     const notes = []
-    localStorage.setItem('notes', JSON.stringify(notes))
+    localStorage.setItem("notes", JSON.stringify(notes))
 
     // Clear filed
     UI.clearFiled()
-
   }
 })
